86af61dc14958ccce2d356415706e6be038be9f5
Jianshuo Yang <jyang63@stevens.edu>
2020-04-26 08:48:22 -0400

    updated some routes code.

---
+++ routes/moments.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const data = require('../data');
+const momentData = data.moments;
+
+router.get('/', (req, res) => {
+
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.post('', (req, res) => {
+
+});
+
+router.put('', (req, res) => {
+
+});
+
+router.delete('', (req, res) => {
+
+});
---
+++ routes/users.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const data = require('../data');
+const userData = data.users;
+
+router.get('/', (req, res) => {
+
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.post('', (req, res) => {
+
+});
+
+router.put('', (req, res) => {
+
+});
+
+router.delete('', (req, res) => {
+
+});
